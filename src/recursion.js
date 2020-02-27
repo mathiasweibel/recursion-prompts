97db4afcf3fee9e29f7bdad759c029f0093f6757
Mathias <mathias2702@gmail.com>
2020-02-27 16:05:20 -0500

    Finish Recursion 9. reverse

--- src/recursion.js
+++ src/recursion.js
@@ -146,6 +146,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string === '') return '';
+  else {
+    return reverse(string.substr(1)) + string[0];
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
